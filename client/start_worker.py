94e8fa1b2eada7b1c6512ecb6e4db062e15fec20
AtsukiTak <takatomgoo@gmail.com>
2018-06-16 16:53:49 +0900

    fix client/start_worker to accept multiple hashtags

--- client/start_worker.py
+++ client/start_worker.py
@@ -3,20 +3,22 @@ import base64
 import json
 import sys
 
-if len(sys.argv) != 4:
-    print('Usage: python %s <url> <img_file> <hashtag>' % sys.argv[0])
+if len(sys.argv) < 4:
+    print('Usage: python %s <url> <img_file> <hashtag> ...' % sys.argv[0])
     quit()
 
 url = sys.argv[1]
 img_file_path = sys.argv[2]
-hashtag = sys.argv[3]
+hashtags = []
+for i in range(3, len(sys.argv)):
+    hashtags.append(sys.argv[i])
 
 print('Reading image file %s' % img_file_path)
 img = open(img_file_path, 'rb').read()
 encoded_img = base64.standard_b64encode(img)
 print('Encoded image into base64')
 
-payload = {'origin_img': encoded_img.decode('utf-8'), 'hashtags': [hashtag]}
+payload = {'origin_img': encoded_img.decode('utf-8'), 'hashtags': hashtags}
 
 res = requests.post(url, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
 
