48e8986e9924ecddbb4f9976d02c650cd1da9a71
AtsukiTak <takatomgoo@gmail.com>
2018-06-16 16:54:07 +0900

    use mongodb instead of diesel

--- migrations/20180611234851_create_insta_posts/down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE insta_posts
--- migrations/20180611234851_create_insta_posts/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE insta_posts (
-    id SERIAL PRIMARY KEY,
-    post_id VARCHAR NOT NULL,
-    user_name VARCHAR NOT NULL,
-    image_url VARCHAR NOT NULL,
-    hashtag VARCHAR NOT NULL,
-)
