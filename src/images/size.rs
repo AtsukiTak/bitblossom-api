14cbc7d68bf2a42c920536b849d856d160040a0a
AtsukiTak <takatomgoo@gmail.com>
2018-06-22 20:34:26 +0900

    add more size type

--- src/images/size.rs
+++ src/images/size.rs
@@ -9,6 +9,7 @@ pub trait MultipleOf<T>: Size {}
 
 pub trait SmallerThan<T>: Size {}
 
+// =========== Piece image's size ===============
 #[derive(Debug, Clone)]
 pub struct Size30x30;
 
@@ -18,7 +19,31 @@ impl Size for Size30x30 {
 }
 
 impl SmallerThan<Size1500x1500> for Size30x30 {}
+impl SmallerThan<Size3000x3000> for Size30x30 {}
 
+#[derive(Debug, Clone)]
+pub struct Size50x50;
+
+impl Size for Size50x50 {
+    const WIDTH: u32 = 50;
+    const HEIGHT: u32 = 50;
+}
+
+impl SmallerThan<Size1500x1500> for Size50x50 {}
+impl SmallerThan<Size3000x3000> for Size50x50 {}
+
+#[derive(Debug, Clone)]
+pub struct Size100x100;
+
+impl Size for Size100x100 {
+    const WIDTH: u32 = 100;
+    const HEIGHT: u32 = 100;
+}
+
+impl SmallerThan<Size1500x1500> for Size100x100 {}
+impl SmallerThan<Size3000x3000> for Size100x100 {}
+
+// =========== Original image's size ===============
 #[derive(Debug, Clone)]
 pub struct Size1500x1500;
 
@@ -28,3 +53,17 @@ impl Size for Size1500x1500 {
 }
 
 impl MultipleOf<Size30x30> for Size1500x1500 {}
+impl MultipleOf<Size50x50> for Size1500x1500 {}
+impl MultipleOf<Size100x100> for Size1500x1500 {}
+
+#[derive(Debug, Clone)]
+pub struct Size3000x3000;
+
+impl Size for Size3000x3000 {
+    const WIDTH: u32 = 3000;
+    const HEIGHT: u32 = 3000;
+}
+
+impl MultipleOf<Size30x30> for Size3000x3000 {}
+impl MultipleOf<Size50x50> for Size3000x3000 {}
+impl MultipleOf<Size100x100> for Size3000x3000 {}
